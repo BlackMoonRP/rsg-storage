255255805de638d8cbe365f8ae7a399ed38b36bc
RexShack <rexshackuk@gmail.com>
2022-10-30 11:53:24 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,20 @@
+# RexshackGaming
+- discord : https://discord.gg/s5uSk56B65
+- youtube : https://www.youtube.com/channel/UCikEgGfXO-HCPxV5rYHEVbA
+- github : https://github.com/Rexshack-RedM
+
+# Framework QRCore RedM Edition
+- https://github.com/QRCore-RedM-Re
+
+# Showcase
+[![Watch the video](https://img.youtube.com/vi/lNaoEGzoHFA/maxresdefault.jpg)](https://youtu.be/lNaoEGzoHFA)
+
+# Dependancies
+- qr-core
+
+# Installation
+- ensure that the dependancies are added and started
+- add rsg-storage to your resources folder
+
+# Starting the resource
+- add the following to your server.cfg file : ensure rsg-storage
---
+++ config.lua
@@ -0,0 +1,40 @@
+Config = {}
+
+Config.ValentineMaxWeight = 4000000
+Config.ValentineMaxSlots = 48
+
+Config.BlackwaterMaxWeight = 4000000
+Config.BlackwaterMaxSlots = 48
+
+Config.StdenisMaxWeight = 4000000
+Config.StdenisMaxSlots = 48
+
+Config.RhodesMaxWeight = 4000000
+Config.RhodesMaxSlots = 48
+
+Config.AnnesburgMaxWeight = 4000000
+Config.AnnesburgMaxSlots = 48
+
+Config.StrawberryMaxWeight = 4000000
+Config.StrawberryMaxSlots = 48
+
+Config.TumbleweedMaxWeight = 4000000
+Config.TumbleweedMaxSlots = 48
+
+Config.Blip = {
+    blipName = 'Storage', -- Config.Blip.blipName
+    blipSprite = -426139257, -- Config.Blip.blipSprite
+    blipScale = 0.2 -- Config.Blip.blipScale
+}
+
+Config.StorageNpc = {
+	
+	[1] = { ["Model"] = "U_M_M_RhdTrainStationWorker_01", ["Pos"] = vector3(-177.8889, 646.84179, 113.58424 -1), ["Heading"] = 59.125999 }, -- valentine storage
+	[2] = { ["Model"] = "U_M_M_RhdTrainStationWorker_01", ["Pos"] = vector3(-734.923, -1254.389, 44.734107 -1), ["Heading"] = 310.59378 }, -- blackwater storage
+	[3] = { ["Model"] = "U_M_M_RhdTrainStationWorker_01", ["Pos"] = vector3(2669.6577, -1500.203, 45.968963 -1), ["Heading"] = 47.808605 }, -- stdenis storage
+	[4] = { ["Model"] = "U_M_M_RhdTrainStationWorker_01", ["Pos"] = vector3(1229.9815, -1279.412, 76.021537 -1), ["Heading"] = 321.43701 }, -- rhodes storage
+	[5] = { ["Model"] = "U_M_M_RhdTrainStationWorker_01", ["Pos"] = vector3(2934.8232, 1306.6634, 44.47974 -1), ["Heading"] = 3.6759085 }, -- annesburg storage
+	[6] = { ["Model"] = "U_M_M_RhdTrainStationWorker_01", ["Pos"] = vector3(-1753.648, -385.3262, 156.49415 -1), ["Heading"] = 241.16018 }, -- strawberry storage
+	[7] = { ["Model"] = "U_M_M_RhdTrainStationWorker_01", ["Pos"] = vector3(-5494.741, -2959.111, -0.69497 -1), ["Heading"] = 343.78573 }, -- tumbleweed storage
+	
+}
